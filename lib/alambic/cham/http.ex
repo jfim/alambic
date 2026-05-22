79a6724b8c084819e3c7556a69229aab10d945f8
Jean-Francois Im <jeanfrancois.im@gmail.com>
2026-05-21 19:19:15 -0700

    feat: Cham.fetch_cleaning_content for content.md artifact

--- lib/alambic/cham/http.ex
+++ lib/alambic/cham/http.ex
@@ -21,4 +21,25 @@ defmodule Alambic.Cham.HTTP do
       {:error, reason} -> {:error, reason}
     end
   end
+
+  @impl Alambic.Cham
+  def fetch_cleaning_content(item_id) do
+    fetch_cleaning_content(item_id,
+      base_url: Application.fetch_env!(:alambic, :cham_base_url),
+      filename: Application.fetch_env!(:alambic, :cham_cleaning_content_filename)
+    )
+  end
+
+  @doc false
+  def fetch_cleaning_content(item_id, opts) do
+    base = Keyword.fetch!(opts, :base_url)
+    filename = Keyword.fetch!(opts, :filename)
+    url = "#{base}/api/v1/items/#{URI.encode(item_id)}/files/#{URI.encode(filename)}"
+
+    case Req.get(url) do
+      {:ok, %Req.Response{status: 200, body: body}} when is_binary(body) -> {:ok, body}
+      {:ok, %Req.Response{status: status}} -> {:error, {:status, status}}
+      {:error, reason} -> {:error, reason}
+    end
+  end
 end
